5b218df9eb0ac30e4054ea1609cfdb57e82c0d4f
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-01-20 19:31:54 -0800

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+#vendor
+git clone https://github.com/LinuxGuy312/android_vendor_realme_RMX1805.git -b oss vendor/realme/RMX1805
+
+#kernel
+git clone https://github.com/LinuxGuy312/android_kernel_realme_RMX1805.git --depth=1 -b ArcticFox kernel/realme/RMX1805
+
+#clang
+git clone --depth=1 https://github.com/DrtSinX98/android_prebuilts_clang_host_linux-x86_clang-6443078 prebuilts/clang/host/linux-x86/clang-6443078
+
+#SEpolicy-um
+git clone https://github.com/LineageOS/android_device_qcom_sepolicy.git -b  lineage-19.0-legacy-um  device/qcom/sepolicy-legacy-um
+
+#opensource
+#git clone https://github.com/LineageOS/android_vendor_qcom_opensource_dataservices.git -b lineage-19.0 vendor/qcom/opensource/dataservices
