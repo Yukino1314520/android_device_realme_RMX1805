944693bc153641dff23ca3f7a6c5a1e96f51a5b3
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-01-20 20:06:29 -0800

    update clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/LinuxGuy312/android_vendor_realme_RMX1805.git -b os
 git clone https://github.com/LinuxGuy312/android_kernel_realme_RMX1805.git --depth=1 -b ArcticFox kernel/realme/RMX1805
 
 #clang
-git clone --depth=1 https://github.com/DrtSinX98/android_prebuilts_clang_host_linux-x86_clang-6443078 prebuilts/clang/host/linux-x86/clang-6443078
+git clone --depth=1 https://github.com/picasso09/clang-11.0.1-r383902.git prebuilts/clang/host/linux-x86/clang-r383902
 
 #SEpolicy-um
 git clone https://github.com/LineageOS/android_device_qcom_sepolicy.git -b  lineage-19.0-legacy-um  device/qcom/sepolicy-legacy-um
