33c0b748f4307e27fcb541d25a5c3c1d0882f0d0
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-01-20 19:06:58 -0800

    Welcome aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX1805.mk
+    $(LOCAL_DIR)/aosp_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_RMX1805-user \
-    arrow_RMX1805-userdebug \
-    arrow_RMX1805-eng
+    aosp_RMX1805-user \
+    aosp_RMX1805-userdebug \
+    aosp_RMX1805-eng
